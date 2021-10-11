e85d458489253567c7dd6a76444c35140c14c26a
LucasDonatti <74219540+LucasDonatti@users.noreply.github.com>
2021-10-11 10:56:55 -0300

    Add projeto ao repositório

---
+++ src/main/java/br/lucas/lemmy/LemmyApplication.java
@@ -0,0 +1,13 @@
+package br.lucas.lemmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/lucas/lemmy/LemmyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lucas.lemmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
