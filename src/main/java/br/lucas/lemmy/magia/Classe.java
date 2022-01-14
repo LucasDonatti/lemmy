825f1253fa9c774ec81d973eee10bf9a35ab462d
LucasDonatti <lucas.frantti@gmail.com>
2022-01-14 09:23:35 -0300

    Add classes ao front

--- src/main/java/br/lucas/lemmy/magia/Classe.java
+++ src/main/java/br/lucas/lemmy/magia/Classe.java
@@ -1,16 +1,12 @@
 package br.lucas.lemmy.magia;
 
 public enum Classe {
-	BÁRBARO,
 	BARDO,
 	BRUXO,
 	CLÉRIGO,
 	DRUIDA,
 	FEITICEIRO,
-	GUERREIRO,
-	LADINO,
 	MAGO,
-	MONGE,
 	PALADINO,
 	PATRULHEIRO
 }
